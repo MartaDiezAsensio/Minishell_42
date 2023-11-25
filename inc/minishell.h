624202c1875502949b0e5eb2c51af7059145520d
mgaspar- <marc.gaspar.martinez@gmail.com>
2023-11-25 14:10:45 +0100

    eco eco

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,18 +6,23 @@
 /*   By: mgaspar- <mgaspar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 13:07:43 by mgaspar-          #+#    #+#             */
-/*   Updated: 2023/11/25 13:33:11 by mgaspar-         ###   ########.fr       */
+/*   Updated: 2023/11/25 13:58:26 by mgaspar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# define EXIT_SUCCESS 1
-# define
-
-# include <stdio.h>
 # include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
+# define EXIT_SUCCESS 0
+# define EXIT_BAD_ARGS 1
 
+typedef struct s_input {
+	// variables que ens calguin del environment?
+}	t_input;
 #endif
--- src/main.c
+++ src/main.c
@@ -6,18 +6,45 @@
 /*   By: mgaspar- <mgaspar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 13:07:14 by mgaspar-          #+#    #+#             */
-/*   Updated: 2023/11/25 13:31:27 by mgaspar-         ###   ########.fr       */
+/*   Updated: 2023/11/25 14:04:40 by mgaspar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+void	init_input(t_input *input)
+{
+	// get env info we need??
+}
+
+int	shell_loop(t_input *input)
+{
+	char	*prompt;
+	char	*parsed_prompt;
+
+	while (1)
+	{
+		printf("\n:) minishell> ");
+		// display prompt
+		prompt = readline(NULL);
+		// wait for command
+		parsed_prompt = prompt;
+		// parse prompt
+		printf("prompt i got: %s", parsed_prompt);
+		// execute command
+		free(prompt);
+	}
+}
+
 int	main(int argc, char **argv, char **env)
 {
-	(void)argc;
-	(void)argv;
-	// get env data you need
-	shell_loop();
-	cleanup();
-	return (SUCCESS);
+	t_input	input;
+	int		exit_code;
+
+	init_input(&input);
+	exit_code = shell_loop(&input);
+	//cleanup??
+	if (exit_code)
+		return (exit_code);
+	return (EXIT_SUCCESS);
 }
