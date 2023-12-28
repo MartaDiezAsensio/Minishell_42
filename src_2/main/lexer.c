558846bf16d254d6c1308af8cc709140749affa1
Marta Diez Asensio <mdiez-as@cbr7s1.42barcelona.com>
2023-12-28 20:39:34 +0100

    comentaris doxygen, afegits

--- src_2/main/lexer.c
+++ src_2/main/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: mdiez-as <mdiez-as@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 16:58:25 by mdiez-as          #+#    #+#             */
-/*   Updated: 2023/12/28 11:33:08 by mdiez-as         ###   ########.fr       */
+/*   Updated: 2023/12/28 20:18:53 by mdiez-as         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 #include "../../include_2/main.h"
 #include "../../include_2/lexer_utils.h"
 
+/**
+ * @brief frees tokens and adjusted tokens
+ * 
+ * @param tokens 
+ * @param adjusted 
+ * @return void* (NULL)
+ */
 static void	*free_tokens(char **tokens, char **adjusted)
 {
 	ft_free_split(tokens);
@@ -22,6 +29,17 @@ static void	*free_tokens(char **tokens, char **adjusted)
 	return (NULL);
 }
 
+/**
+ * @brief	Get the next token object
+ * 			Extracts a sequence of consecutive characters (run) from the beggining of a token
+ * 			1. (Determine special)
+ * 			2. If the character *tmp is the same as the ddeermined "next_speial" character,
+ * 			a loop is entered to copy the consecutive identical characters into next_token
+ * 			3. A new token tmp is created by extracting the remaininng characters into next_token  
+ * 
+ * @param token 
+ * @return char * (next_token)
+ */
 static char	*get_next_token(char **token)
 {
 	char	*next_token;
@@ -30,40 +48,56 @@ static char	*get_next_token(char **token)
 	int		i;
 
 	tmp = *token;
+	// Determine the special character that constitutes the run
 	next_special = get_next_special(tmp);
+	// Allocate memory for the next_token
 	next_token = ft_calloc(get_next_token_size(tmp, next_special) + 1, \
 			sizeof(*next_token));
 	if (!next_token)
 		return (NULL);
 	
 	i = 0;
+	// If the current char is the next_special char, copy the run
 	if (*tmp == next_special)
 		while (*tmp && *tmp == next_special)
 			next_token[i++] = *tmp++;
 	
+	// Create a new token without the extracted run
 	tmp = ft_substr(*token, i, ft_strlen(*token));
 	if (*token == NULL)
 		return (ft_free((void **)&next_token));
+	// Free the original token and update it to the new token
 	free(*token);
 	*token = tmp;
 	return (next_token);
 }
 
+/**
+ * @brief	Get the subshell token object
+ * 			Extracts and concatenates tokens that belong to a subshell.
+ * 
+ * @param tokens 
+ * @param i (i[0] index)
+ * @return char* (subshell token)
+ */
 static char	*get_subshell_token(char *tokens[], int *i)
 {
 	char	*subshell_token;
 	int		subshell_count;
 
 	subshell_token = NULL;
-	subshell_count = get_subshell_count(tokens, *i);
+	subshell_count = get_subshell_count(tokens, *i); // determine count of subshells at a given position of the array.
 	while (tokens[*i] && subshell_count)
 	{
+		// Check if this is the last token of the subshell
 		if (subshell_count == 1 && ft_strchr(tokens[*i], ')') \
 		&& ft_strlen(ft_strrchr(tokens[*i], ')')) != 1)
 			return (get_end_of_subshell(tokens, i, subshell_token));
+		// Append the curren token to the subshell_token
 		subshell_token = ft_append(&subshell_token, tokens[*i]);
 		if (!subshell_token)
 			return (NULL);
+		// Adjust the subshell count based on the presence on '(' or ')'
 		if (ft_strchr(tokens[*i], '(') || ft_strchr(tokens[*i], ')'))
 			subshell_count--;
 		(*i)++;
@@ -71,6 +105,15 @@ static char	*get_subshell_token(char *tokens[], int *i)
 	return (subshell_token);
 }
 
+/**
+ * @brief	Token adjustment based on these conditions:
+ * 				1. If the first character of a token is '(', the function assumes it is the start of a subshell.
+ * 				2. If a token is not the start of a subshell and is not considered unadjusted, the function duplicates the token using ft_strdup.
+ * 				3. If a token is unadjusted, the function calls get_next_token to obtain the next token.
+ * 
+ * @param tokens 
+ * @return char** (adjusted tokens)
+ */
 static char	**adjust_tokens(char **tokens)
 {
 	char	**adjusted;
@@ -102,6 +145,19 @@ static char	**adjust_tokens(char **tokens)
 	return (adjusted);
 }
 
+/**
+ * @brief	1. checks line syntax
+ * 			2. split, creates tokens from line
+ * 			3. (join quotes) exit if it fails
+ * 			4. (adjust tokens) exit if it fails
+ * 			5. (set lex)
+ * 			6. check token syntax
+ * 			7. Exit status, reurn, = parser
+ * 			8. (reset lex)
+ * 
+ * @param line 
+ * @return int (status)
+ */
 int	lexer(char *line)
 {
 	char	**tokens;
--- src_2/main/utils/lexer_utils/check_line_syntax.c
+++ src_2/main/utils/lexer_utils/check_line_syntax.c
@@ -6,7 +6,7 @@
 /*   By: mdiez-as <mdiez-as@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 17:20:54 by mdiez-as          #+#    #+#             */
-/*   Updated: 2023/12/11 18:01:49 by mdiez-as         ###   ########.fr       */
+/*   Updated: 2023/12/28 20:38:08 by mdiez-as         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 #include "../../../../include_2/main.h"
 #include "../../../../include_2/lexer_utils.h"
 
-
+/**
+ * @brief Exit on syntax error and free
+ * 
+ * @return int (exit status)
+ */
 int	exit_on_syntax_error(void)
 {
 	ft_fprintf(2, "minishell: Invalid Syntax at token\n");
@@ -23,15 +27,35 @@ int	exit_on_syntax_error(void)
 	return (EXIT_SYNTAX_ERROR);
 }
 
+/**
+ * @brief	If the conditions are met (i.e., the character at 
+ * 			position i is not '&' and not '|'), the function returns true. 
+ * 			Otherwise, it returns false.
+ * 
+ * @param line 
+ * @param i 
+ * @return true (if there are & or | chars)
+ * @return false (if not)
+ */
 static bool	is_correct_subshell(char *line, int i)
 {
+	// Move back through whitespace chars
 	while (i > 0 && line[i] && ft_isspace(line[i]))
 		i--;
+	// Check if cha at pos i is not '&' or '|'
 	if (i >= 0 && (line[i] != '&' && line[i] != '|'))
 		return (false);
 	return (true);
 }
 
+/**
+ * @brief chck if line at position i has opening brackets.
+ * 
+ * @param line 
+ * @param i 
+ * @return true 
+ * @return false 
+ */
 static bool	has_opening_bracket(char *line, int i)
 {
 	while (i > 0 && line[i] && line[i] != '(')
@@ -41,6 +65,14 @@ static bool	has_opening_bracket(char *line, int i)
 	return (true);
 }
 
+/**
+ * @brief	Count the occurrences of a character 'c' whithin the line.
+ * 			Skips counting occurences of characters within single or double quotes.
+ * 
+ * @param line 
+ * @param c 
+ * @return int (c occurences)
+ */
 static int	count_subshell_chars(char *line, char c)
 {
 	int	i;
@@ -50,9 +82,11 @@ static int	count_subshell_chars(char *line, char c)
 	count = 0;
 	while (line[i])
 	{
+		// If the current character is a single quote ('), skip characters until the next single quote
 		if (line[i] == '\'')
 			while (line[++i] && line[i] != '\'')
 				;
+		// Same for doubble quotes
 		if (line[i] == '\"')
 			while (line[++i] && line[i] != '\"')
 				;
@@ -64,16 +98,28 @@ static int	count_subshell_chars(char *line, char c)
 	return (count);
 }
 
+/**
+ * @brief	Checks syntax of line.
+ * 			It primarily focuses on ensuring balanced parentheses and checks for 
+ * 			specific conditions related to single quotes, double quotes, parentheses,
+ * 			semicolons, and backslashes.
+ * 
+ * @param line 
+ * @return true 
+ * @return false 
+ */
 bool	is_valid_line_syntax(char *line)
 {
 	int	i;
 
 	i = 0;
+	// Check weether all opened brackets are closed
 	if (count_subshell_chars(line, '(') != count_subshell_chars(line, ')'))
 		return (false);
 	
 	while (line[i])
 	{
+		// Check if quotes are properly closed
 		if (line[i] == '\'')
 			while (line[++i] && line[i] != '\'')
 				;
@@ -94,18 +140,18 @@ bool	is_valid_line_syntax(char *line)
 	return (true);
 }
 
-int	main()
-{
-	char	line[] = "echo Hello";
-	char	c = 'l';
-	int		i = 5;
-	int		count;
-	bool	res;
+// int	main()
+// {
+// 	char	line[] = "echo Hello";
+// 	char	c = 'l';
+// 	int		i = 5;
+// 	int		count;
+// 	bool	res;
 
-	count = count_subshell_chars(line, c);
-	//res = has_opening_bracket(line, i);
+// 	count = count_subshell_chars(line, c);
+// 	//res = has_opening_bracket(line, i);
 
-	res = is_correct_subshell(line, c);
-	printf("res = %d\n", res);
-}
+// 	res = is_correct_subshell(line, c);
+// 	printf("res = %d\n", res);
+// }
 
